c2c7eaac3addb40d98552dc3c56e9e06e964eb81
Andrey <krepezh_plus@mail.ru>
2025-07-26 13:10:30 +0500

    Completed background module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import './styles.css'
 
+
+
+
+
+
+
+import { RandomColorBackgroundModule } from './modules/background.module'
+
+const backgroundModule = new RandomColorBackgroundModule()
+
+menu.add(backgroundModule)
