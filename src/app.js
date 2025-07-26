213d8a1994aab7081c8e97c608fcdaf4ee347383
Andrey Lakhno <Lakhno63@mail.ru>
2025-07-26 13:49:01 +0300

    Add random sound modul

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import './styles.css'
+import { ContextMenu } from './menu'
 
+import { RandomSound } from './modules/randomSound.module'
+
+const contextMenu = new ContextMenu('#menu')
+
+const randomSound = new RandomSound()
+contextMenu.add(randomSound)
