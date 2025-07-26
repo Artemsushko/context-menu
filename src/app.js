4fc0c411b17c62776707cc7b6b0be145e68fd42d
Andrey-G1Thub <andreyrukshenas@gmail.com>
2025-07-26 13:44:08 +0300

    add feature/clicks-module

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-import './styles.css';
-import { ContextMenu } from './menu';
+import './styles.css'
+import { ContextMenu } from './menu'
+import { ClicksModule } from './modules/clicks.module.js'
 
-const contextMenu = new ContextMenu('#menu');
+const contextMenu = new ContextMenu('#menu')
+const clicksModule = new ClicksModule(
+  'click-analytics',
+  'Отслеживание кликов',
+  5
+)
+//вызываем метод ослеживания кликов
+clicksModule.trigger()
+contextMenu.add(clicksModule)
