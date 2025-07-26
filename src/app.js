d4374f8091ccfbace9fce438bf115ff390bf1dd9
Andrey <krepezh_plus@mail.ru>
2025-07-26 14:22:46 +0500

    update completed shape module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import './styles.css'
-
+import { ContextMenu } from './menu';
 import { ShapeModule } from './modules/shape.module'
-
+const contextMenu = new ContextMenu('#menu');
 const figureModule = new ShapeModule()
 
 contextMenu.add(figureModule)
