42c422ce96afaeb7bf6c157380ae303842a07b7c
Andrey <krepezh_plus@mail.ru>
2025-07-26 12:05:09 +0500

    Implemented background module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,12 @@
 import {Module} from '../core/module'
 
-export class BackgroundModule extends Module {
+export class RandomColorBackgroundModule extends Module {
+    constructor() {
+        super('random bg', 'Поменять цвет')
+    }
 
+    trigger() {
+        const color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+        document.body.style.backgroundColor = color
+    }
 }
