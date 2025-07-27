dfb79b5c223eb82afe1d96c44237694fba8728ef
Artem Sushko <artem.sushko.98@gmail.com>
2025-07-27 02:31:02 +0200

    change name of variable

--- src/modules/randomSound.module.js
+++ src/modules/randomSound.module.js
@@ -1,26 +1,26 @@
-import { Module } from '../core/module'
-import { random } from '../utils'
-import applepay from '../../assets/sounds/applepay.mp3'
-import tada from '../../assets/sounds/quottadamquot-sound.mp3'
-import icq from '../../assets/sounds/single-sound-message-icq-ooh.mp3'
-import done from '../../assets/sounds/sound-when-something-is-done-on-the-computer-informative.mp3'
+import { Module } from '../core/module';
+import { random } from '../utils';
+import applepay from '../../assets/sounds/applepay.mp3';
+import tada from '../../assets/sounds/quottadamquot-sound.mp3';
+import icq from '../../assets/sounds/single-sound-message-icq-ooh.mp3';
+import done from '../../assets/sounds/sound-when-something-is-done-on-the-computer-informative.mp3';
 
 export class RandomSound extends Module {
   constructor() {
-    super('random-sound', 'Рандомный звук')
-    this.soundsFile = [applepay, tada, icq, done]
+    super('random-sound', 'Рандомный звук');
+    this.soundsFile = [applepay, tada, icq, done];
   }
-  
-  #randomSound(fileArrey) {
-    return fileArrey[random(0, fileArrey.length - 1)]
+
+  #randomSound(fileArray) {
+    return fileArray[random(0, fileArray.length - 1)];
   }
 
   trigger() {
     try {
-      const audio = new Audio(this.#randomSound(this.soundsFile))
-      audio.play()
+      const audio = new Audio(this.#randomSound(this.soundsFile));
+      audio.play();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 }
